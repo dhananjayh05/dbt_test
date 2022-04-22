51aa336a6eb62b8b4c0c258c34d1dd8dec9aff9a
kkshetty <karthik.shetty@jiva.ag>
2022-04-22 12:37:38 +0530

    created new incremental  model Metabase Harvest Import

---
+++ models/dbt_test_model_1.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+with source_data as (
+
+    SELECT
+	    *
+    FROM
+	    marvin_gcppsql_public.dispatches
+    WHERE
+	    adjustments > 0
+
+)
+
+select *
+from source_data
