c99294d24ccc0bd2524cd5a273cc47d9a0ed211e
dhananjayh05 <120562449+dhananjayh05@users.noreply.github.com>
2023-01-05 12:16:17 +0530

    edited harvest model

--- models/metabase/harvests.sql
+++ models/metabase/harvests.sql
@@ -1,38 +1,15 @@
-{{
-    config(
-        schema='metabase',
-        materialized='incremental',
-        unique_key='id',
-        post_hook="delete from {{ this }} where id not in (select id from marvin_gcppsql_public.harvests where _fivetran_deleted IS NULL)" 
-    )
-}}
+{{ config(materialized='table') }}
 
+with source_data as (
 
-SELECT
-    id,
-    display_id,
-    farmer_id,
-    created_by,
-    moisture_level,
-    base_price,
-    grade,
-    grade_price_adjustment,
-    net_price,
-    gross_weight,
-    total_bags,
-    provided_bags,
-    gross_value,
-    provided_bag_cost,
-    outstanding_loan_amount_at_harvest,
-    loan_repayment_amount,
-    net_payout AS farmer_net_payout,
-    loan_deferral_amount,
-    created_at,
-    updated_at,
-    dispatch_id,
-    confirmed_at,
-    force_confirmed_by
-FROM
-    marvin_gcppsql_public.harvests
-WHERE
-    _fivetran_deleted IS NULL
+    SELECT
+	    *
+    FROM
+	    marvin_gcppsql_public.dispatches
+    WHERE
+	    adjustments > 0
+
+)
+
+select *
+from source_data
